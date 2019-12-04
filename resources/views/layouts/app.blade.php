d956ebc2c9d8a8ead45af16fd9f5304e32524639
Sayan Kanatov <spiritoftimes@mail.ru>
2019-12-04 18:38:10 +0600

    replaced TEST on Test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    @if(env('APP_NAME') == 'TEST')
+    @if(env('APP_NAME') == 'Test')
     <meta name="robots" content="noindex, nofollow" />
     @endif
 
