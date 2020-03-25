f00586244824b43b95111db2cff28d1a7b5453ab
Sayan Kanatov <sayankanatov@Sayans-MacBook-Pro.local>
2020-03-25 20:51:19 +0600

    dskjdksdjksa

--- resources/views/green/pages/news/index.blade.php
+++ resources/views/green/pages/news/index.blade.php
@@ -9,7 +9,7 @@
    	<div class="search1">
  		<form class="catalin" action="{{route('news.search')}}" method="post">
  			@csrf
- 			<input type="text" class="searchTerm" placeholder="Поиск записи">
+ 			<input type="text" class="searchTerm" placeholder="Поиск записи" name="search">
           	<button type="submit" class="searchButton1">
             	<i class="fa fa-search"></i>
          	</button>
@@ -24,7 +24,7 @@
 		@foreach($news as $item)
 			<div class="news-block__item">
 				<a href="{{route('news.show',['alias' => $item->alias])}}" class="news-block__img">
-					<img src="/{{$item->image}}" alt="{{$item->title}}">
+					<img src="/{{$item->image}}" alt="{{$item->title}}" class="photonews">
 				</a>
 				<a href="{{route('news.show',['alias' => $item->alias])}}" class="news-block__title">
 					<h3>{{$item->title}}</h3>
