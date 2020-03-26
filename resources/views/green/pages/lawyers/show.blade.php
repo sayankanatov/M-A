e620f817ea4023e01a5d705bc9afc20221c4ebaf
Sayan Kanatov <sayankanatov@Sayans-MacBook-Pro.local>
2020-03-26 23:04:18 +0600

    added lawyer mobile

--- resources/views/green/pages/lawyers/show.blade.php
+++ resources/views/green/pages/lawyers/show.blade.php
@@ -114,80 +114,92 @@
 							</div>
 						</div>
 					</div>
-		{{-- <div class="lawyers_mobile">
+		<div class="lawyers_mobile">
 			<div class="container">
 				<div class="lowers_mobile card">
 					<div class="row">
 						<div class="col-md-4 col-6">
 							<div class="lawyers_photo">
-								<a href="yurist.html"><img src="image/Lawyers/Rectangle 26.png" alt="photo"></a>
-		
+								<img src="{{$lawyer->image ? '/'.$lawyer->image : asset('front2/img/spec-item.png')}}" alt="photo">
 							</div>
 						</div>
 						<div class="col-md-6 col-6">
 							<div class="lawyers_name">
-								<a href="yurist.html">Муканов Мурат Муканович</a>
+								{{$lawyer->fullname($lawyer->id)}}
 		
 							</div>
 							<div class="lawyers_stars">
-								<img class="star" src="image/Lawyers/icon/Icon_star.svg" alt="Звезда">
-								<img class="star" src="image/Lawyers/icon/Icon_star.svg" alt="Звезда">
-								<img class="star" src="image/Lawyers/icon/Icon_star.svg" alt="Звезда">
-								<img class="star" src="image/Lawyers/icon/Icon_star.svg" alt="Звезда">
-								<img class="star" src="image/Lawyers/icon/Icon_star.svg" alt="Звезда">
+								<img class="star" src="{{asset('front3/image/Lawyers/icon/Icon_star.svg')}}" alt="Звезда">
+								<img class="star" src="{{asset('front3/image/Lawyers/icon/Icon_star.svg')}}" alt="Звезда">
+								<img class="star" src="{{asset('front3/image/Lawyers/icon/Icon_star.svg')}}" alt="Звезда">
+								<img class="star" src="{{asset('front3/image/Lawyers/icon/Icon_star.svg')}}" alt="Звезда">
+								<img class="star" src="{{asset('front3/image/Lawyers/icon/Icon_star.svg')}}" alt="Звезда">
 								<span>5.0</span>
 							</div>
 							<div class="lawyers_rewyes">
-								<img src="image/Lawyers/icon/Vector.svg" alt="rewyes">
-								<a href="#"><span class="rewyes_text">7 отзывов</span></a>
+								<img src="{{asset('front3/image/Lawyers/icon/Vector.svg')}}" alt="rewyes">
+								<a href="#"><span class="rewyes_text">{{$lawyer->feedbacks()->count()}} отзывов</span></a>
 							</div>
 						</div>
 					</div>
 					<div class="row">
 						<div class="col-12">
 							<div class="lawyers_specialization">
-								<img src="image/Lawyers/icon/icon4.svg" alt="Icon">
+								<img src="{{asset('front3/')}}image/Lawyers/icon/icon4.svg" alt="Icon">
 								Специализация:
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="lawyers_uslugi">
-								<span class="spec">Уголовное право</span>
-								<span class="spec">Земельное право</span>
-								<span class="spec">Семейное право</span>
-								<span class="spec">Арбитраж</span>
-								<span class="spec">Корпоративное право</span>
-								<span class="spec">Алименты</span>
-								<span class="spec">Наследство</span>
+								@foreach($lawyer->services as $service)
+								<a href="{{route('service',['city'=>$city->alias,'id'=>$service->alias])}}">
+									<span class="spec">{{$service->name_ru}}</span>
+								</a>
+                                @endforeach
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="lawyers_adress">
-								<img src="image/Lawyers/icon/icon3.svg" alt="Icon">
-								Нур-Султан (Астана), г. Нур-Султан, пр. Женис, 20 (ВП-1)
+								<img src="{{asset('front3/image/Lawyers/icon/icon3.svg')}}" alt="Icon">
+								{{$lawyer->address}}
 							</div>
 						</div>
 						<div class="col-12">
 							<div class="lawyers_graphick">
-								<img src="image/Lawyers/icon/icon2.svg" alt="Icon">
+								<img src="{{asset('front3/image/Lawyers/icon/icon2.svg')}}" alt="Icon">
 								График работы: <span>9:00 - 18:00</span>
 								<div class="lawyers_week">
-									<span class="weekdays">ПН</span>
-									<span class="weekdays">ВТ</span>
-									<span class="weekdays">СР</span>
-									<span class="weekdays">ЧТ</span>
-									<span class="weekdays">ПТ</span>
-									<span class="output">СБ</span>
-									<span class="output">ВС</span></div>
+									<span class="{{$lawyer->monday == 1 ? 'weekdays' : 'output'}}">
+				                                        пн
+				                                    </span>
+				                                    <span class="{{$lawyer->tuesday == 1 ? 'weekdays' : 'output'}}">
+				                                        вт
+				                                    </span>
+				                                    <span class="{{$lawyer->wednesday == 1 ? 'weekdays' : 'output'}}">
+				                                        ср
+				                                    </span>
+				                                    <span class="{{$lawyer->thursday == 1 ? 'weekdays' : 'output'}}">
+				                                        чт
+				                                    </span>
+				                                    <span class="{{$lawyer->friday == 1 ? 'weekdays' : 'output'}}">
+				                                        пт
+				                                    </span>
+				                                    <span class="{{$lawyer->saturday == 1 ? 'weekdays' : 'output'}}">
+				                                        сб
+				                                    </span>
+				                                    <span class="{{$lawyer->sunday == 1 ? 'weekdays' : 'output'}}">
+				                                        вс
+				                                    </span>
+								</div>
 							</div>
 						</div>
 						<div class="col-md-12 col-12">
 							<div class="lawyers_consultation">
-								<div class="yes">
+								<div class="{{$lawyer->is_free ? 'yes' : 'no'}}">
 									Бесплатная консультация:
 									<span>
-										<img src="image/Lawyers/icon/Cons_galka.svg" alt="Icon">
-										Есть
+										<img src="{{asset('front3/image/Lawyers/icon/Cons_galka.svg')}}" alt="Icon">
+										{{$lawyer->is_free ? 'Есть' : 'Нет'}}
 									</span>
 								</div>
 							</div>
@@ -195,7 +207,11 @@
 						<div class="col-md-12 col-12">
 							<div class="lawyers_phone">
 								<div class="phone">
-								<div class="phone"><span class="hide-tailMobile">7 777 111 22 23</span> <a href="#" class="click-telMobile">Показать</a>
+								<div class="phone">
+									<a href="tel:{{$lawyer->telephone}}">
+										<span class="hide-tailMobile">{{$lawyer->telephone}}</span>
+									</a>
+									<a href="#" class="click-telMobile">Показать</a>
 								</div>
 								</div>
 							</div>
@@ -204,8 +220,8 @@
 							<div class="lawyers_whtasapp">
 								<div class="whtasapp">
 		
-									<a href="#"><img src="image/Lawyers/icon/icon_WhatsApp.svg" alt="Icon"></a>
-									<a href="#" class="whtasapp-text">WhatsApp</a>
+									<a href="#"><img src="{{asset('front3/image/Lawyers/icon/icon_WhatsApp.svg')}}" alt="Icon"></a>
+									<a rel="nofollow" target="_blank" href="https://api.whatsapp.com/send?phone={{$lawyer->telephone}}&text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5,%20%D1%8F%20%D1%81%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%20Yuristy.kz.%20%D0%A5%D0%BE%D1%87%D1%83%20%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%86%D0%B8%D1%8E." class="whtasapp-text">WhatsApp</a>
 									<span></span>
 								</div>
 							</div>
@@ -213,7 +229,7 @@
 					</div>
 				</div>
 			</div>
-		</div> --}}
+		</div>
 				</div>
 			</div>
 		</div>
