e278ff9ecca79a42aeaa947db961a28a7dd5ca31
Sayan Kanatov <sayankanatov@Sayans-MacBook-Pro.local>
2020-05-26 18:53:24 +0600

    fixing export 3

--- app/Http/Controllers/API/ExportExcelController.php
+++ app/Http/Controllers/API/ExportExcelController.php
@@ -36,8 +36,6 @@ class ExportExcelController extends Controller
             $headers = Config::get('excel.headers');
             $exportData = (array)json_decode($request->get('data'),true);
 
-            dd($exportData);
-
             $export = new StudentExport([
                 $headers,
                 $exportData,
